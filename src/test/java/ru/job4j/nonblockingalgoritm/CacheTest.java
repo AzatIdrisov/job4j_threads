91cc9e89b5fce5c7876f7b20c08bb955d8f109be
Azat Idrisov <azzzat72@yandex.ru>
2021-03-25 21:21:48 +0500

    1. Неблокирующий кеш [#495484]

--- src/test/java/ru/job4j/nonblockingalgoritm/CacheTest.java
+++ src/test/java/ru/job4j/nonblockingalgoritm/CacheTest.java
@@ -1,31 +1,20 @@
 package ru.job4j.nonblockingalgoritm;
 
-import junit.framework.TestCase;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.is;
-
-import java.util.concurrent.atomic.AtomicReference;
 
 public class CacheTest {
 
-    @Test
+    @Test (expected = OptimisticException.class)
     public void whenTryChangeOneModel() throws InterruptedException {
-        AtomicReference<Exception> exception = new AtomicReference<>();
         Cache cache = new Cache();
         cache.add(new Base(1, 1));
-        Thread thread = new Thread(() -> cache.update(new Base(1, 1)));
-        thread.start();
-        thread.join();
-        thread = new Thread(() -> {
-            try {
-                cache.update(new Base(1, 2));
-            } catch (OptimisticException e) {
-                exception.set(e);
-            }
-        });
-        thread.start();
-        thread.join();
-        assertThat(exception.get().getMessage(), is("Versions are not same"));
+        cache.update(new Base(1, 1));
+        cache.update(new Base(1, 3));
+        Thread first = new Thread(() -> cache.update(new Base(1, 1)));
+        first.start();
+        first.join();
+        Thread second = new Thread(() -> cache.update(new Base(1, 5)));
+        second.start();
+        second.join();
     }
 }
